114d7c393435b160be3eb61f6c581fe618e7755c
Masha <kazantsevamaria@mail.ru>
2021-08-27 09:52:08 -0300

    Downloading options from Django admin added for classes.

--- classes/admin.py
+++ classes/admin.py
@@ -1,4 +1,8 @@
+import pandas as pd
 from django.contrib import admin
+from django.http import HttpResponse
+
+from student_database.settings import BASE_DIR
 from .models import Classes
 from .forms import ClassesForm
 from revenue.models import Revenue
@@ -17,6 +21,27 @@ class ClassesAdmin(admin.ModelAdmin):
     filter_horizontal = ["students"]
     inlines = [RevenueInline]
     form = ClassesForm
+    actions = ["download_excel"]
+
+    def download_excel(self, request, queryset):
+        list_class = []
+        for i in queryset:
+            list_class.append(' ')
+            list_class.append("Группа: " + i.name)
+            for s in i.students.all():
+                list_class.append(s.name)
+        result = pd.DataFrame({list_class[0]: list_class[1:]})
+        result.to_excel(BASE_DIR / "class.xlsx")
+        with open(BASE_DIR / "class.xlsx", "rb") as f:
+            response = HttpResponse(f.read())
+        response['Content-Type'] = 'text/plain,charset=utf8'
+        response['Content-Disposition'] = "attachment; filename=class.xlsx"
+        return response
+
+
+
+
+
 
 
 admin.site.register(Classes, ClassesAdmin)
